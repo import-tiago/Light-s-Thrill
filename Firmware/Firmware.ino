4b27869fa6ee2285c3b452880cc9910b0efc5020
Tiago Silva <tiagodepaulasilva@gmail.com>
2020-07-13 20:33:07 -0300

    Minimum RGB and PWM/MOSFET firmware test

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,24 +1,57 @@
-int LED = 3;
+int RED = 5;
+int BLUE = 3;
+int GREEN = 6;
+
 int PWM = 0;
   
 void setup() { 
-    pinMode(LED, OUTPUT);
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
 
-    digitalWrite(LED, HIGH);
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+
+    digitalWrite(RED, HIGH);
     delay(500);
-    digitalWrite(LED, LOW);
+    digitalWrite(RED, LOW);
     delay(500);
     
-    digitalWrite(LED, HIGH);
+    digitalWrite(GREEN, HIGH);
     delay(500);
-    digitalWrite(LED, LOW);
+    digitalWrite(GREEN, LOW);
+    delay(500);
+       
+    digitalWrite(BLUE, HIGH);
+    delay(500);
+    digitalWrite(BLUE, LOW);
     delay(500);
     
 }
  
 void loop() {
+        digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
     for(PWM = 0; PWM < 256; PWM++) {
-      analogWrite(LED, PWM); 
+      analogWrite(RED, PWM); 
+      delay(10); 
+    }
+    
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+        for(PWM = 0; PWM < 256; PWM++) {
+      analogWrite(GREEN, PWM); 
+      delay(10); 
+    }
+    
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+        for(PWM = 0; PWM < 256; PWM++) {
+      analogWrite(BLUE, PWM); 
       delay(10); 
     }
 }
