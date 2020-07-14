e335824cc1f51bffb802a9031b453f70a0a6718a
Tiago Silva <tiagodepaulasilva@gmail.com>
2020-07-13 21:36:38 -0300

    Minimum RGBLED Library firmware test

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,57 +1,32 @@
-int RED = 5;
-int BLUE = 3;
-int GREEN = 6;
+#include <RGBLED.h>
+
+#define RED  5
+#define GREEN 6
+#define BLUE 3
+
+RGBLED LED_Strip(RED, GREEN, BLUE, COMMON_CATHODE);
 
 int PWM = 0;
-  
-void setup() { 
-    pinMode(RED, OUTPUT);
-    pinMode(GREEN, OUTPUT);
-    pinMode(BLUE, OUTPUT);
 
-    digitalWrite(RED, LOW);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
+void setup() {
 
-    digitalWrite(RED, HIGH);
-    delay(500);
-    digitalWrite(RED, LOW);
-    delay(500);
-    
-    digitalWrite(GREEN, HIGH);
-    delay(500);
-    digitalWrite(GREEN, LOW);
-    delay(500);
-       
-    digitalWrite(BLUE, HIGH);
-    delay(500);
-    digitalWrite(BLUE, LOW);
-    delay(500);
-    
 }
- 
+
 void loop() {
-        digitalWrite(RED, LOW);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
-    for(PWM = 0; PWM < 256; PWM++) {
-      analogWrite(RED, PWM); 
-      delay(10); 
-    }
-    
-    digitalWrite(RED, LOW);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
-        for(PWM = 0; PWM < 256; PWM++) {
-      analogWrite(GREEN, PWM); 
-      delay(10); 
-    }
-    
-    digitalWrite(RED, LOW);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, LOW);
-        for(PWM = 0; PWM < 256; PWM++) {
-      analogWrite(BLUE, PWM); 
-      delay(10); 
-    }
+
+  for (PWM = 0; PWM < 256; PWM++) {
+    LED_Strip.writeRGB(PWM, 0, 0);
+    delay(10);
+  }
+
+  for (PWM = 0; PWM < 256; PWM++) {
+    LED_Strip.writeRGB(0, PWM, 0);
+    delay(10);
+  }
+
+  for (PWM = 0; PWM < 256; PWM++) {
+    LED_Strip.writeRGB(0, 0, PWM);
+    delay(10);
+  }
+
 }
